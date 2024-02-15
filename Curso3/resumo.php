df5b768fc1658f1cb6950e1f13313e4d19217b67
Lucas do Amaral Faustino <lukinhafaustino2001@gmail.com>
2024-02-14 22:46:48 -0300

    refresh

---
+++ Curso3/resumo.php
@@ -0,0 +1,15 @@
+<?php
+
+$array = [
+    0 => 'zero',
+    1 => 'um',
+    2 => 'dois'
+];
+
+var_dump ($array);
+
+foreach ($array as $numeral => $nomeNumero) {
+    echo "$numeral em portugês é: $nomeNumero" . PHP_EOL;
+};
+
+echo "Total: " . count($array).PHP_EOL; // Pega o tamanho do array
